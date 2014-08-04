f3deb399e34785ca3ebdb168a64e1ff40707ba4c
zzw19880707 <zzw414851474@qq.com>
2014-08-04 17:05:50 +0800

    加入地图定位类

--- 开发/petinfo/duoduo/AppDelegate.h
+++ 开发/petinfo/duoduo/AppDelegate.h
@@ -7,9 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
+#import "BMapKit.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate,BMKGeneralDelegate>
+{
+    BMKMapManager* _mapManager;
+}
 @property (strong, nonatomic) UIWindow *window;
 
 @end
