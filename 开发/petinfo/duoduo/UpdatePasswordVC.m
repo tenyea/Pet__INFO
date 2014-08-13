70370d1244b78fa374825c66cc1f6cdb36281568
zzw19880707 <zzw414851474@qq.com>
2014-08-13 08:58:19 +0800

    添加校验逻辑

--- 开发/petinfo/duoduo/UpdatePasswordVC.m
+++ 开发/petinfo/duoduo/UpdatePasswordVC.m
@@ -10,9 +10,7 @@
 #define passwordRules @"密码长度为6-16"
 #define passwordAgainRules @"确认密码不正确"
 @interface UpdatePasswordVC ()
-{
-    NSString *registeredMessage;
-}
+
 @end
 
 @implementation UpdatePasswordVC
@@ -46,43 +44,63 @@
     [self popVC];
 }
 -(void)submitAction{
-    if([firstPass.text isEqualToString:secondPass.text])
+    
+    if ( originalPass.text.length<6||originalPass.text.length>16)
     {
-        if (registeredMessage) {
-            return;
-        }
-        NSMutableDictionary *dic = [[NSMutableDictionary alloc]init];
-        [dic setValue:firstPass.text forKey:@"password"];
-        [dic setValue:[[NSUserDefaults standardUserDefaults] stringForKey:UD_userID_Str] forKey:@"userId"];
-        [self showHudInBottom:@"提交中。"];
-        [self getDate:URL_UpdatePassword andParams:dic andcachePolicy:1 success:^(AFHTTPRequestOperation *operation, id responseObject) {
-            [self removeHUD];
-            int code =[[responseObject objectForKey:@"code"] intValue];
-            if ( code == 0) {
-                [self showHudInBottom:@"修改成功"];
-                [self performSelector:@selector(removeHUD) withObject:nil afterDelay:1.5];
-                [self performSelector:@selector(popVC) withObject:nil afterDelay:1.5];
-#warning !!!!
-            }else if(code == 1001){
-                originalLabel.text = @"原密码错误";
-            }else if(code == 1000){
-                [self removeHUD];
-                [self showHudInBottom:@"修改失败"];
-                [self performSelector:@selector(removeHUD) withObject:nil afterDelay:1.5];
-            }
-        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-            _po([error localizedDescription]);
+        originalLabel.text=passwordRules;
+        return;
+    }else
+    {
+        originalLabel.text=nil;
+    }
+    
+    if (firstPass.text.length<6||firstPass.text.length>16)
+    {
+        firstLabel.text=passwordRules;
+        return;
+    }else
+    {
+        firstLabel.text=nil;
+    }
+    
+    if (![secondPass.text isEqualToString:firstPass.text])
+    {
+        secondLabel.text=passwordAgainRules;
+        return;
+    }else
+    {
+        secondLabel.text=nil;
+    }
+    
+    NSMutableDictionary *dic = [[NSMutableDictionary alloc]init];
+    [dic setValue:firstPass.text forKey:@"password"];
+    [dic setValue:originalPass.text forKey:@"oldPassword"];
+    [dic setValue:[[NSUserDefaults standardUserDefaults] stringForKey:UD_userID_Str] forKey:@"userId"];
+    [self showHudInBottom:@"提交中。"];
+    [self getDate:URL_UpdatePassword andParams:dic andcachePolicy:1 success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [self removeHUD];
+        int code =[[responseObject objectForKey:@"code"] intValue];
+        if ( code == 0) {
+            [self showHudInBottom:@"修改成功"];
+            [self performSelector:@selector(removeHUD) withObject:nil afterDelay:1.5];
+            [self performSelector:@selector(popVC) withObject:nil afterDelay:1.5];
+        }else if(code == 1011){
+            originalLabel.text = @"原密码错误";
+        }else if(code == 1000){
             [self removeHUD];
             [self showHudInBottom:@"修改失败"];
             [self performSelector:@selector(removeHUD) withObject:nil afterDelay:1.5];
-//            [self popVC];
-        }];
-        
-        
-    }else{
-        _po(@"确认密码不正确");
-        secondLabel.text=passwordAgainRules;
-    }
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        _po([error localizedDescription]);
+        [self removeHUD];
+        [self showHudInBottom:@"修改失败"];
+        [self performSelector:@selector(removeHUD) withObject:nil afterDelay:1.5];
+        //            [self popVC];
+    }];
+    
+    
+    
     
 }
 
@@ -93,8 +111,8 @@
     {
         if ( originalPass.text.length<6||originalPass.text.length>16)
         {
-            registeredMessage=passwordRules;
             originalLabel.text=passwordRules;
+            return NO;
         }else
         {
             originalLabel.text=nil;
@@ -104,8 +122,8 @@
     {
         if (firstPass.text.length<6||firstPass.text.length>16)
         {
-            registeredMessage=passwordRules;
             firstLabel.text=passwordRules;
+            return NO;
         }else
         {
             firstLabel.text=nil;
@@ -115,9 +133,8 @@
     {
         if (![secondPass.text isEqualToString:secondPass.text])
         {
-            registeredMessage =passwordAgainRules;
-            NSLog(@"确认密码不正确");
             secondLabel.text=passwordAgainRules;
+            return NO;
         }else
         {
             secondLabel.text=nil;
@@ -128,6 +145,43 @@
     return YES;
 }
 
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField{
+    if (textField.tag==90002)
+    {
+        if ( originalPass.text.length<6||originalPass.text.length>16)
+        {
+            originalLabel.text=passwordRules;
+            return NO;
+        }else
+        {
+            originalLabel.text=nil;
+        }
+    }else if (textField.tag==90003)
+    {
+        if (firstPass.text.length<6||firstPass.text.length>16)
+        {
+            firstLabel.text=passwordRules;
+            return NO;
+        }else
+        {
+            firstLabel.text=nil;
+        }
+    }else if (textField.tag==90004)
+    {
+        if (![secondPass.text isEqualToString:firstPass.text])
+        {
+            secondLabel.text=passwordAgainRules;
+            return NO;
+        }else
+        {
+            secondLabel.text=nil;
+        }
+        
+    }
+    
+    
+    return YES;
+}
 -(void)popVC {
     //    返回上一级菜单
     [self.navigationController popViewControllerAnimated:YES];
