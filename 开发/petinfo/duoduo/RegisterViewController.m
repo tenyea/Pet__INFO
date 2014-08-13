14814f823a7e517db28680197db3964572dcacc9
zzw19880707 <zzw414851474@qq.com>
2014-08-13 08:58:50 +0800

    添加校验逻辑

--- 开发/petinfo/duoduo/RegisterViewController.m
+++ 开发/petinfo/duoduo/RegisterViewController.m
@@ -19,9 +19,6 @@
 #define emailEexisting @"邮箱已经注册"
 #define userNameConditions @"用户名只能是数字、字母、下划线"
 @interface RegisterViewController ()
-{
-    NSString *registeredMessage;
-}
 @end
 
 @implementation RegisterViewController
@@ -57,7 +54,44 @@
     passwordTF.secureTextEntry=YES;
     passwordAgainTF.secureTextEntry=YES;
 }
-
+- (BOOL)textFieldShouldEndEditing:(UITextField *)textField{
+    if(userNameTF.text.length<4||userNameTF.text.length>10)
+    {
+        
+        userNameLabel.text=userNameRules;
+        return NO;
+    }else
+    {
+        userNameLabel.text=nil;
+    }
+    if(![self validateEmail:emailTF.text])
+    {
+        NSLog(@"邮箱不合法");
+        emailLabel.text=emailRules;
+        return NO;
+    }else
+    {
+        emailLabel.text=nil;
+    }
+    
+    if (passwordTF.text.length<6||passwordTF.text.length>16)
+    {
+        passwordLabel.text=passwordRules;
+        return NO;
+    }else
+    {
+        passwordLabel.text=nil;
+    }
+    if (![passwordTF.text isEqualToString:passwordAgainTF.text])
+    {
+        passwordAgainLabel.text=passwordAgainRules;
+        return NO;
+    }else
+    {
+        passwordAgainLabel.text=nil;
+    }
+    return YES;
+}
 // 键盘下一步，返回操作
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
@@ -65,8 +99,9 @@
     {
         if(userNameTF.text.length<4||userNameTF.text.length>10)
         {
-            registeredMessage=userNameRules;
+            
             userNameLabel.text=userNameRules;
+            return NO;
         }else
         {
             userNameLabel.text=nil;
@@ -76,9 +111,9 @@
     {
         if(![self validateEmail:emailTF.text])
         {
-            registeredMessage =emailRules;
             NSLog(@"邮箱不合法");
             emailLabel.text=emailRules;
+            return NO;
         }else
         {
             emailLabel.text=nil;
@@ -91,8 +126,8 @@
     {
         if (passwordTF.text.length<6||passwordTF.text.length>16)
         {
-            registeredMessage=passwordRules;
             passwordLabel.text=passwordRules;
+            return NO;
         }else
         {
             passwordLabel.text=nil;
@@ -102,15 +137,15 @@
     {
         if (![passwordTF.text isEqualToString:passwordAgainTF.text])
         {
-            registeredMessage =passwordAgainRules;
-            NSLog(@"确认密码不正确");
             passwordAgainLabel.text=passwordAgainRules;
+            return NO;
         }else
         {
             passwordAgainLabel.text=nil;
         }
         
         [passwordAgainTF resignFirstResponder];
+        [self submit];
     }    return YES;
 }
 // 邮箱格式判断
@@ -123,81 +158,112 @@
 
 // 注册按钮 点击事件
 - (IBAction)registerBtn:(id)sender {
-    registeredMessage = nil;
+    [self submit];
+}
+// 注册成功取消HUD,跳转界面
+-(void)timerFiredSuccess:(NSTimer *)timer{
+    [self removeHUD];
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+
+
+-(void)submit{
     userNameLabel.text=nil;
     passwordLabel.text=nil;
     passwordAgainLabel.text=nil;
     emailLabel.text=nil;
     if([passwordAgainTF.text isEqualToString:passwordTF.text])
     {
-        if (!registeredMessage) {
-            
-            NSMutableDictionary *dic = [[NSMutableDictionary alloc]init];
-            [dic setValue:userNameTF.text forKey:@"username"];
-            [dic setValue:passwordTF.text forKey:@"password"];
-            [dic setValue:emailTF.text forKey:@"email"];
-            
-            [self getDate:URL_RegisterServlet andParams:dic andcachePolicy:1 success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                NSString *code = [responseObject objectForKey:@"code"];
-                NSLog(@"code = %@",code);
-                
-                int a = [code intValue];
-                if(a==0)
-                {
-                    [self showHudInBottom:@"注册成功"];
-#warning 未登录
-                    [self performSelector:@selector(timerFiredSuccess:) withObject:nil afterDelay:1.5];
-                    return ;
-                }else if(a==1001)
-                {
-                    registeredMessage =parametersLost;
-                    NSLog(@"请输入完整信息");
-                    
-                }else if(a==1003)
-                {
-                    registeredMessage =usernamEexisting;
-                    NSLog(@"用户名已经注册");
-                }else if(a==1004)
-                {
-                    registeredMessage =emailEexisting;
-                    NSLog(@"邮箱已经注册");
-                    emailLabel.text=emailEexisting;
-                }else if(a==1005)
-                {
-                    registeredMessage =userNameRules;
-                    NSLog(@"用户名4-10位");
-                    userNameLabel.text=userNameRules;
-                }else if(a==1006)
-                {
-                    registeredMessage =passwordRules;
-                    NSLog(@"密码（6-16位）");
-                    passwordLabel.text=passwordRules;
-                }else if(a==1007)
-                {
-                    registeredMessage =userNameConditions;
-                    NSLog(@"用户名非法");
-                    userNameLabel.text=userNameConditions;
-                }else if(a==1008)
-                {
-                    registeredMessage =emailRules;
-                    NSLog(@"邮箱格式不正确");
-                    emailLabel.text=emailRules;
-                }
-            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                [error localizedFailureReason];
-                _po([error localizedDescription]);
-            }];
+        
+        if(userNameTF.text.length<4||userNameTF.text.length>10)
+        {
+            userNameLabel.text=userNameRules;
+            return ;
+        }else
+        {
+            userNameLabel.text=nil;
         }
+        
+        if(![self validateEmail:emailTF.text])
+        {
+            emailLabel.text=emailRules;
+            return;
+        }else
+        {
+            emailLabel.text=nil;
+        }
+        
+        if (passwordTF.text.length<6||passwordTF.text.length>16)
+        {
+            passwordLabel.text=passwordRules;
+            return ;
+        }else
+        {
+            passwordLabel.text=nil;
+        }
+        if (![passwordTF.text isEqualToString:passwordAgainTF.text])
+        {
+            passwordAgainLabel.text=passwordAgainRules;
+            return ;
+        }else
+        {
+            passwordAgainLabel.text=nil;
+        }
+        
+        
+        NSMutableDictionary *dic = [[NSMutableDictionary alloc]init];
+        [dic setValue:userNameTF.text forKey:@"username"];
+        [dic setValue:passwordTF.text forKey:@"password"];
+        [dic setValue:emailTF.text forKey:@"email"];
+        
+        [self getDate:URL_RegisterServlet andParams:dic andcachePolicy:1 success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSString *code = [responseObject objectForKey:@"code"];
+            NSLog(@"code = %@",code);
+            
+            int a = [code intValue];
+            if(a==0)
+            {
+                [self showHudInBottom:@"注册成功"];
+                [self performSelector:@selector(timerFiredSuccess:) withObject:nil afterDelay:1.5];
+                return ;
+            }else if(a==1001)
+            {
+                //                    registeredMessage =parametersLost;
+                NSLog(@"请输入完整信息");
+                
+            }else if(a==1003)
+            {
+                userNameLabel.text = usernamEexisting;
+                NSLog(@"用户名已经注册");
+            }else if(a==1004)
+            {
+                NSLog(@"邮箱已经注册");
+                emailLabel.text=emailEexisting;
+            }else if(a==1005)
+            {
+                NSLog(@"用户名4-10位");
+                userNameLabel.text=userNameRules;
+            }else if(a==1006)
+            {
+                NSLog(@"密码（6-16位）");
+                passwordLabel.text=passwordRules;
+            }else if(a==1007)
+            {
+                NSLog(@"用户名非法");
+                userNameLabel.text=userNameConditions;
+            }else if(a==1008)
+            {
+                NSLog(@"邮箱格式不正确");
+                emailLabel.text=emailRules;
+            }
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            [error localizedFailureReason];
+            _po([error localizedDescription]);
+        }];
     }else {
         _po(@"确认密码不正确");
         passwordAgainLabel.text=passwordAgainRules;
     }
-    
-}
-// 注册成功取消HUD,跳转界面
--(void)timerFiredSuccess:(NSTimer *)timer{
-    [self removeHUD];
-    [self.navigationController popToRootViewControllerAnimated:YES];
 }
 
 @end
