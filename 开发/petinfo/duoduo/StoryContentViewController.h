0963b866327613c67e49488d4f1b2f60073e2272
zzw19880707 <zzw414851474@qq.com>
2014-08-26 14:53:43 +0800

    重写布局，加入三个按钮的事件

--- 开发/petinfo/duoduo/StoryContentViewController.h
+++ 开发/petinfo/duoduo/StoryContentViewController.h
@@ -6,18 +6,26 @@
 //  Copyright (c) 2014年 zzw. All rights reserved.
 //
 
-#import "TenyeaBaseViewController.h"
 
-@interface StoryContentViewController : TenyeaBaseViewController
-@property (strong, nonatomic) IBOutlet UIScrollView *backgroundScrollView;
+#import "TenyeaNoBarVC.h"
+/**
+ *  故事页的内容页。主要显示图片和文字。有评论、喜欢、分享三个按钮
+ */
+@interface StoryContentViewController : TenyeaNoBarVC
+@property (strong, nonatomic)  UIScrollView *backgroundScrollView;
 
-@property (strong, nonatomic) IBOutlet UIButton *likeButton;
-@property (strong, nonatomic) IBOutlet UIImageView *contentImageView;
-@property (nonatomic,copy)NSString *petPresentation;
-@property (nonatomic,copy)NSString *petImageViewURL;
-@property (nonatomic,copy)NSString *photoID;
-- (IBAction)commentsButton:(id)sender;
-- (IBAction)likeButton:(id)sender;
-- (IBAction)shareButton:(id)sender;
+@property (strong, nonatomic)  UIImageView *contentImageView;
 
+@property (nonatomic,strong)NSString *petPresentation;
+@property (nonatomic,strong)NSString *petImageViewURL;
+
+/**
+ *  通过
+ *
+ *  @param imageId     当前页面的id
+ *  @param hasComments yes：用评论按钮  no：没有按钮评论
+ *
+ *  @return id
+ */
+-(id)initWithID:(NSString *)imageId hasComments:(BOOL)hasComments;
 @end
--- 开发/petinfo/duoduo/StoryContentViewController.m
+++ 开发/petinfo/duoduo/StoryContentViewController.m
@@ -9,90 +9,203 @@
 #import "StoryContentViewController.h"
 #import "UIImageView+WebCache.h"
 #import "LoginViewController.h"
+#import "StoryCommentsViewController.h"
+#import <ShareSDK/ShareSDK.h>
 @interface StoryContentViewController ()
 {
     UILabel *contentLabel;
+    NSString *petImageId;
+    float height;
+    BOOL isGood ;//是否点赞 0:未点赞 1 点赞
+    BOOL _hasComments;
+    
+    UILabel *_animateLabel;
 }
 @end
 
 @implementation StoryContentViewController
-@synthesize likeButton=_likeButton;
-@synthesize photoID=_photoID;
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+/**
+ *  通过
+ *
+ *  @param imageId     当前页面的id
+ *  @param hasComments yes：用评论按钮  no：没有按钮评论
+ *
+ *  @return id
+ */
+-(id)initWithID:(NSString *)imageId hasComments:(BOOL)hasComments{
+    self = [super init];
     if (self) {
-        // Custom initialization
+        petImageId = imageId;
+        height= 0 ;
+        _hasComments = hasComments;
     }
     return self;
+    
 }
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    // 根据内容大小设置label大小
-    contentLabel = [[UILabel alloc] initWithFrame:CGRectZero];
-    contentLabel.text=_petPresentation;
-    contentLabel.font=[UIFont systemFontOfSize:12];
-       [self.view addSubview:contentLabel];
-    // 计算内容大小
-    CGSize contentSize = [contentLabel.text sizeWithFont:contentLabel.font constrainedToSize:CGSizeMake(300, 10000) lineBreakMode:NSLineBreakByWordWrapping];
-   
     
-    contentLabel.numberOfLines = 0;
-   
-    _po(_petPresentation);
-    
-  
-    // Do any additional setup after loading the view from its nib.
-    _backgroundScrollView.contentSize=CGSizeMake(320, 20000);
-    NSURL *url = [NSURL URLWithString:_petImageViewURL];
-     [_contentImageView setImageWithURL:url placeholderImage:nil];
-    if ([_petImageViewURL isEqualToString:@""]) {
-        contentLabel.frame = CGRectMake(10, 74, contentSize.width, contentSize.height);
-       
-    }else
-    {
-         contentLabel.frame = CGRectMake(10, 210, contentSize.width, contentSize.height);
-    
-    }
-    
-    
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)commentsButton:(id)sender {
-   }
-
-- (IBAction)likeButton:(id)sender {
     NSUserDefaults *myUserDefaults = [NSUserDefaults standardUserDefaults];
     
-    // 读取
+    // 读取参数
     NSNumber *readCount = [myUserDefaults objectForKey:UD_userID_Str];
     NSMutableDictionary *dic = [[NSMutableDictionary alloc]init];
-    [dic setValue:_photoID forKey:@"petPhotoId"];
+    [dic setValue:petImageId forKey:@"petPhotoId"];
     [dic setValue:readCount forKey:@"userId"];
-    
-    
-    [self getDate:URL_Like andParams:dic andcachePolicy:1 success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    _backgroundScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, ScreenWidth, ScreenHeight)];
+    [self.view addSubview:_backgroundScrollView];
+//    解析数据
+    [self getDate:URL_Pet_Photo andParams:dic andcachePolicy:1 success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSString *code =[responseObject objectForKey:@"code"];
-        NSLog(@"%@",code);
-        
         int a = [code intValue];
         if(a==0)
         {
-            _po(@"111");
-            _likeButton.enabled=NO;
-//            _likeButton.titleLabel.font=[UIFont systemFontOfSize:8];
-//            _likeButton.titleLabel.text=@"不喜欢";
+            height = 10;
+            NSDictionary *dic=[responseObject objectForKey:@"petPhoto"];
+            if (dic == [NSNull null]) {
+                self.bgStr = Tenyea_str_load_error;
+                return ;
+            }
+            self.petImageViewURL=[dic objectForKey:@"petPhotoImg"];
             
-            NSLog(@"登录成功");
+            self.petPresentation=[dic objectForKey:@"petPhotoText"];
+//            照片
+            if (![_petImageViewURL isEqualToString:@""]) {
+                _contentImageView = [[UIImageView alloc]initWithFrame:CGRectMake(40, 20, ScreenWidth - 80, 120)];
+                [_backgroundScrollView addSubview:_contentImageView];
+                [_contentImageView setImageWithURL:[NSURL URLWithString:_petImageViewURL]];
+                height += 140;
+            }
+//            文字描述
+            if (![_petPresentation isEqualToString:@""]) {
+                // 根据内容大小设置label大小
+                contentLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, height, ScreenWidth - 40, 0)];
+                contentLabel.text=_petPresentation;
+                contentLabel.font=[UIFont systemFontOfSize:12];
+                contentLabel.numberOfLines = 0;
+//                计算高度
+                CGSize size = [_petPresentation boundingRectWithSize:CGSizeMake(ScreenWidth - 40, 0) options:NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:12]} context:nil].size;
+                contentLabel.height = size.height;
+                [_backgroundScrollView addSubview:contentLabel];
+                height += size.height + 20;
+
+            }
+//            是否有评论
+            if (_hasComments) {
+                NSArray *arr = @[@"评论",@"喜欢"];//,@"分享"
+                for (int i = 0 ; i < arr.count;  i ++ ) {
+                    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(20 + i *80, height, 60, 30)];
+                    button.tag = 100 + i ;
+                    button.backgroundColor = [UIColor redColor];
+                    button.titleLabel.font = [UIFont boldSystemFontOfSize:15];
+                    [button addTarget:self action:@selector(selectAction:) forControlEvents:UIControlEventTouchUpInside];
+                    [button setTitle:arr[i] forState:UIControlStateNormal];
+                    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+                    [_backgroundScrollView addSubview:button];
+                }
+            }
+            else{
+                NSArray *arr = @[@"喜欢"];//,@"分享"
+                for (int i = 0 ; i < arr.count;  i ++ ) {
+                    UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(20 + i *80, height, 60, 30)];
+                    button.tag = 101 + i ;
+                    button.backgroundColor = [UIColor redColor];
+                    button.titleLabel.font = [UIFont boldSystemFontOfSize:15];
+                    [button addTarget:self action:@selector(selectAction:) forControlEvents:UIControlEventTouchUpInside];
+                    [button setTitle:arr[i] forState:UIControlStateNormal];
+                    [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+                    [_backgroundScrollView addSubview:button];
+                }
+            }
+            
+            height += 50;
+            isGood = [[responseObject objectForKey:@"isGood"] boolValue];
+//            未点赞
+            if ( !isGood ) {
+                
+            }
+//            已点赞
+            else{
+                UIButton *button = (UIButton *)VIEWWITHTAG(_backgroundScrollView, 101);
+                [button setTitle:@"已喜欢" forState:UIControlStateNormal];
+                [button setBackgroundColor:[UIColor grayColor]];
+            }
+        }else if(a==1001)
+        {
+            self.bgStr = Tenyea_str_load_error;
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", [error localizedDescription]);
+        self.bgStr = Tenyea_str_load_error;
+    }];
+    
+}
+
+/**
+ *  按钮点击事件
+ *
+ *  @param button 点击的按钮
+ */
+- (void)selectAction:(UIButton *)button {
+    
+    switch (button.tag) {
+        case 100:{
+            [self commentsAction];
+            break;
+        }
+        case 101:{
+            
+            [self likeAction];
+            break;
+        }
+        case 102:{
+            [self shareAction];
+            break;
+        }
+            
+        default:
+            break;
+    }
+}
+/**
+ *  进入评论页
+ */
+-(void)commentsAction{
+    [self.navigationController pushViewController:[[StoryCommentsViewController alloc] initWithPetId:petImageId] animated:YES];
+}
+/**
+ *  点赞的事件
+ */
+- (void)likeAction {
+    NSUserDefaults *myUserDefaults = [NSUserDefaults standardUserDefaults];
+    
+    // 读取
+    NSString *userId = [myUserDefaults objectForKey:UD_userID_Str];
+    NSMutableDictionary *dic = [[NSMutableDictionary alloc]init];
+    [dic setValue:petImageId forKey:@"petPhotoId"];
+    [dic setValue:userId forKey:@"userId"];
+    
+    
+    [self getDate:URL_Like andParams:dic andcachePolicy:1 success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSString *code =[responseObject objectForKey:@"code"];        
+        int a = [code intValue];
+        if(a==0)
+        {
+            isGood = !isGood;
+            if ( !isGood) {
+                UIButton *button = (UIButton *)VIEWWITHTAG(_backgroundScrollView, 101);
+                [button setTitle:@"喜欢" forState:UIControlStateNormal];
+                [button setBackgroundColor:[UIColor redColor]];
+                [self labelAnimate:@"-1"];
+            }else{
+                UIButton *button = (UIButton *)VIEWWITHTAG(_backgroundScrollView, 101);
+                [button setTitle:@"已喜欢" forState:UIControlStateNormal];
+                [button setBackgroundColor:[UIColor grayColor]];
+                [self labelAnimate:@"+1"];
+
+            }
         }else
         {
             // 去登陆  缺少userID
@@ -101,11 +214,66 @@
         }
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
+        self.bgStr = Tenyea_str_load_error;
     }];
     
 
 }
-
-- (IBAction)shareButton:(id)sender {
+/**
+ *  分享事件
+ */
+- (void)shareAction {
+    NSString *imagePath = [[NSBundle mainBundle] pathForResource:@"ShareSDK"  ofType:@"jpg"];
+    
+    //构造分享内容
+    id<ISSContent> publishContent = [ShareSDK content:@"分享内容"
+                                       defaultContent:@"默认分享内容，没内容时显示"
+                                                image:[ShareSDK imageWithPath:imagePath]
+                                                title:@"ShareSDK"
+                                                  url:@"http://www.sharesdk.cn"
+                                          description:@"这是一条测试信息"
+                                            mediaType:SSPublishContentMediaTypeNews];
+    
+    [ShareSDK showShareActionSheet:nil
+                         shareList:nil
+                           content:publishContent
+                     statusBarTips:YES
+                       authOptions:nil
+                      shareOptions: nil
+                            result:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo> error, BOOL end) {
+                                if (state == SSResponseStateSuccess)
+                                {
+                                    NSLog(@"分享成功");
+                                }
+                                else if (state == SSResponseStateFail)
+                                {
+                                    NSLog(@"分享失败,错误码:%d,错误描述:%@", [error errorCode], [error errorDescription]);
+                                }
+                            }];
 }
+/**
+ *  点赞的动画，label消失的动画
+ *
+ *  @param text 消失的文字（-1/+1）
+ */
+-(void)labelAnimate :(NSString *)text{
+    if (!_animateLabel) {
+        UIButton *button = (UIButton *)VIEWWITHTAG(_backgroundScrollView, 101);
+        _animateLabel =[[UILabel alloc]init];
+        _animateLabel.font = [UIFont systemFontOfSize:12];
+        _animateLabel.textColor = [UIColor grayColor];
+        _animateLabel.height = 13;
+        _animateLabel.width = 12;
+        _animateLabel.right = button.width;
+        [button addSubview:_animateLabel];
+    }
+    _animateLabel.top = -13;
+    _animateLabel.alpha = 1;
+    _animateLabel.text = text;
+    [UIView animateWithDuration:1.2 animations:^{
+        _animateLabel.top =  - 23;
+        _animateLabel.alpha = 0;
+    } ];
+}
+
 @end
